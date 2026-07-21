a8c731f667943924dfc97496f92d467f42c3b789
PolskiEMS <info@polskiems.pl>
2026-07-21 20:07:51 +0200

    feat: add profitability center home page

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import CentrumRentownosciPanel from "../components/dashboard/CentrumRentownosciPanel";
+
+export default function HomePage() {
+  return (
+    <main className="app-shell">
+      <CentrumRentownosciPanel />
+    </main>
+  );
+}
