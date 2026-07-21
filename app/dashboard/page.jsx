cbb67a03b2abfe1777d7e64668447586cc2d5114
PolskiEMS <info@polskiems.pl>
2026-07-21 20:29:11 +0200

    Przenieś panel Centrum Rentowności pod dashboard

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import CentrumRentownosciPanel from "../../components/dashboard/CentrumRentownosciPanel";
+
+export default function DashboardPage() {
+  return (
+    <main className="app-shell">
+      <CentrumRentownosciPanel />
+    </main>
+  );
+}
